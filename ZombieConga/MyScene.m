c51b7cba75f4ff054ab5ab92d6733084365fbe89
Randy Wills <runrandy91@gmail.com>
2013-10-20 14:43:53 -0500

    Created and added sprite to the scene

--- ZombieConga/MyScene.m
+++ ZombieConga/MyScene.m
@@ -14,6 +14,13 @@
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         self.backgroundColor = [SKColor whiteColor];
+        
+        // CREATE A SPRITE
+        SKSpriteNode *bg = [SKSpriteNode spriteNodeWithImageNamed:@"background"];
+        
+        // ADD THE SPRITE TO THE SCENE
+        [self addChild:bg];
+        
     }
     return self;
 }
